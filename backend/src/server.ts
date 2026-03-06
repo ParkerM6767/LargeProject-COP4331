4f69ab8596924976a9c59ca055d4235a68ff6194
Catalyst4 <catalyst4222@gmail.com>
2026-03-06 17:38:19 -0500

    Docker project setup

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+// app.use(bodyParser);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.get("/api/hello", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(5000);
---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+console.log(process.env);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    hmr: {
+      host: "localhost", // The host the browser uses to connect to the dev server
+      clientPort: 5173, // The port the browser will use for HMR websocket connection
+    },
+    watch: {
+      usePolling: true, // Recommended for some Docker setups, especially on Windows/macOS
+    },
+  },
+});
