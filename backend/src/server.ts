b9a8bb3fca3e04cb927880e00c7651c867a5c0b9
Catalyst4 <catalyst4222@gmail.com>
2026-03-13 12:49:57 -0400

    Add mongodb to backend and compose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,8 +9,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.get("/api/hello", (req, res) => {
+// Disables CORS issues
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS",
+  );
+  next();
+});
+
+app.get("/api/hello", async (req, res) => {
   res.status(200).send("Hello World");
 });
 
+console.log("Starting backend");
 app.listen(8000);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-console.log(process.env);
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
