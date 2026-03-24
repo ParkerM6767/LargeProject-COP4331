eef42b7eb5b57efe99b2cc0855c16724e08570b7
insanefire10 <36336394+insanefire10@users.noreply.github.com>
2026-03-24 19:19:10 -0400

    bug fixes to create user

--- backend/src/controllers/user/createUsers.ts
+++ backend/src/controllers/user/createUsers.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
-import "../../db";
+import { client } from "../../db";
 import bcrypt from "bcrypt";
 const SALT_ROUNDS = 10;
+const db = client.db("sitedata")
 
-export function createUser(req: Request, res: Response) {
-    console.log(req.body);
-    let fname: string = req.body.first_name;
+export async function createUser(req: Request, res: Response) {
+    let fname: String = req.body.first_name;
     let lname: string = req.body.last_name;
     let email: string = req.body.email;
     let password_unhashed: string = req.body.password;
-    const password_hashed = bcrypt.hash(password_unhashed, SALT_ROUNDS);
+    let password_hashed = await bcrypt.hash(password_unhashed, SALT_ROUNDS);
     console.log(`Plaintext: ${password_unhashed}\nHashed: ${password_hashed}`);
+    const user_collection = db.collection('users');
     
     res.status(200).json({message:`Post Made by ${fname}`});
 }
