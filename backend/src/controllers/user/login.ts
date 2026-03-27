76fb32b04ea25d27a7ce55c5dc495ec9b67aee2e
ParkerM6767 <parker@lamay.systems>
2026-03-27 14:49:33 -0400

    Added JWT_SECRET to compose and ProcessEnv type

--- backend/src/controllers/user/login.ts
+++ backend/src/controllers/user/login.ts
@@ -2,25 +2,32 @@ import bcrypt from 'bcrypt'
 import { Request, Response } from "express";
 import { User } from '../../models/user.model';
 import jwt from 'jsonwebtoken';
-const maxTokenAge = 60*60*1000; // 1 Hour
+const maxTokenAge = 60 * 60 * 1000; // 1 Hour
 
-export async function login(req: Request, res: Response){
-    const email = req.body.email;
-    const password_unhashed = req.body.password;
+export async function login(req: Request, res: Response) {
+    try {
+        const email = req.body.email;
+        const password_unhashed = req.body.password;
 
-    const user_query = await User.findOne( {email} );
-    if(!user_query) return res.status(400).json({ "message":"invalid credentials" });
+        const user_query = await User.findOne({ email });
+        if (!user_query) return res.status(400).json({ "message": "invalid credentials" });
 
-    const correct_credentials: boolean = await bcrypt.compare(password_unhashed, user_query.password);
-    if(!correct_credentials) return res.status(400).json( {"message":"invalid credentials"} );
+        const correct_credentials: boolean = await bcrypt.compare(password_unhashed, user_query.password);
+        if (!correct_credentials) return res.status(400).json({ "message": "invalid credentials" });
 
-    const token = jwt.sign({ id: user_query._id}, process.env.JWT_SECRET as string, { expiresIn: '1h'} );
-    res.cookie('token', token, {
-        httpOnly: true,
-        sameSite: 'strict',
-        maxAge: maxTokenAge
-    });
-    res.status(200).json( {
-        id: user_query._id
-    } );
+        // hardcoded JWT secret until fix
+        const token = jwt.sign({ id: user_query._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: maxTokenAge
+        });
+        res.status(200).json({
+            id: user_query._id
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`
+        })
+    }
 }
--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DB_HOST: string;
+      JWT_SECRET: string;
     }
     interface ProcessEnv {
       DB_URL: string;
