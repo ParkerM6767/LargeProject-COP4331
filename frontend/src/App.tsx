8f671045d5d63bb6bb16594f78616df6b1d46bdc
Catalyst4 <catalyst4222@gmail.com>
2026-03-27 22:43:13 -0400

    Add in a sidebar with placeholder elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import "./index.css";
 import { LoginModal } from "./components/LoginModal";
 import { Button } from "./components/ui/button";
 import { Dialog, DialogTrigger } from "./components/ui/dialog";
+import { PostSidebar, SideBarToggle } from "./components/PostSidebar";
+import { SidebarInset, SidebarProvider } from "./components/ui/shad-sidebar";
 
 const UCFLong = 28.60235;
 const UCFLat = -81.2002;
@@ -44,33 +46,43 @@ function App() {
   );
 
   return (
-    <div className="w-screen h-screen absolute top-0 left-0">
-      <Map posts={fetchedPosts}>
-        <MapZoom />
+    <SidebarProvider className="w-screen h-screen absolute top-0 left-0">
+      <PostSidebar posts={fetchedPosts} />
 
-        {isLoggedIn ? (
-          <Button
-            onClick={() => setIsLoggedIn(false)}
-            size="lg"
-            className="absolute top-6 right-10 p-4 bg-orange-500"
-          >
-            Logout
-          </Button>
-        ) : (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button
-                size="lg"
-                className="absolute top-6 right-10 p-4 bg-orange-500"
-              >
-                Login
-              </Button>
-            </DialogTrigger>
-            <LoginModal onLoginSuccess={() => setIsLoggedIn(true)} />
-          </Dialog>
-        )}
-      </Map>
-    </div>
+      <SidebarInset className="h-full">
+        {/* Everything that gets moved by the sidebar goes below */}
+
+        <Map posts={fetchedPosts}>
+          <div className="flex gap-2">
+            <SideBarToggle />
+            <MapZoom />
+            {/* Darkmode here */}
+          </div>
+
+          {isLoggedIn ? (
+            <Button
+              onClick={() => setIsLoggedIn(false)}
+              size="lg"
+              className="absolute top-6 right-10 p-4 bg-orange-500"
+            >
+              Logout
+            </Button>
+          ) : (
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  size="lg"
+                  className="absolute top-6 right-10 p-4 bg-orange-500"
+                >
+                  Login
+                </Button>
+              </DialogTrigger>
+              <LoginModal onLoginSuccess={() => setIsLoggedIn(true)} />
+            </Dialog>
+          )}
+        </Map>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
 
