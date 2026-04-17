24199d35896da3555a287ef4a244848737b010e1
DiegoT0r0_2005 <di879891@ucf.edu>
2026-04-16 20:38:19 -0400

    Update deletePosts.ts

--- backend/src/controllers/post/deletePosts.ts
+++ backend/src/controllers/post/deletePosts.ts
@@ -2,11 +2,7 @@ import { Request, Response } from 'express'
 import { Post } from '../../models/post.model'
 import fs from 'fs';
 import path from 'path';
-<<<<<<< HEAD
-const staticImagePath = (import.meta.dirname) + '/../../../public/images/posts/';
-=======
 const staticImagePath = path.join(process.cwd(), 'public/images/posts/')
->>>>>>> 0147656d60d09627c88345f0c96c512de75e7a7f
 
 // DELETE /:id
 export async function deletePost (req: Request, res: Response) {
