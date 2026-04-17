7415d23ae400427a804c71e87ef5017a3273a797
Luke Kacprowski <lukekac@me.com>
2026-04-16 21:23:43 -0400

    Add optionalAuth middleware and apply to GETs

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,10 +1,15 @@
 // Router and middleware functions
 import { Router } from 'express'
 import { authMiddleware } from '../middleware/auth.middleware'
+import { optionalAuthMiddleware } from '../middleware/optionalAuth.middleware'
 
 // Post Functions
 import { createPost } from '../controllers/post/createPosts'
-import { getPosts, getPostById, getPostsByUserId } from '../controllers/post/getPosts'
+import {
+  getPosts,
+  getPostById,
+  getPostsByUserId
+} from '../controllers/post/getPosts'
 import {
   updatePost,
   upvotePost,
@@ -18,11 +23,11 @@ import { storeImage } from '../controllers/post/imageManagement'
 const postrouter = Router()
 
 postrouter.post('/', authMiddleware, storeImage, createPost)
-postrouter.get('/', authMiddleware, getPosts)
+postrouter.get('/', optionalAuthMiddleware, getPosts)
 postrouter.get('/my-posts', authMiddleware, getPostsByUserId)
-postrouter.get('/:id', authMiddleware, getPostById)
-postrouter.put('/:id', authMiddleware, updatePost) 
-postrouter.put('/:id/upvote', authMiddleware, upvotePost) 
+postrouter.get('/:id', optionalAuthMiddleware, getPostById)
+postrouter.put('/:id', authMiddleware, updatePost)
+postrouter.put('/:id/upvote', authMiddleware, upvotePost)
 postrouter.put('/:id/downvote', authMiddleware, downvotePost)
 postrouter.delete('/:id', authMiddleware, deletePost)
 
