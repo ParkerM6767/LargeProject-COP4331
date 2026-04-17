da2462c50586d4d5054b6ef87f4ef4fa3fda80b0
Luke Kacprowski <lukekac@me.com>
2026-04-16 23:12:31 -0400

    updated jest.setup.ts env variables

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -5,14 +5,14 @@ jest.mock('./src/db', () => ({
 }))
 
 beforeAll(() => {
-  process.env.TOP_LEFT_CORNER_BOUNDS = '90,180'
-  process.env.BOTTOM_RIGHT_CORNER_BOUNDS = '-90,-180'
+  process.env.TOP_RIGHT_CORNER_BOUNDS = '90,180'
+  process.env.BOTTOM_LEFT_CORNER_BOUNDS = '-90,-180'
 })
 
 afterAll(async () => {
   if (mongoose.connection.readyState !== 0) {
     await mongoose.connection.close()
   }
-  delete process.env.TOP_LEFT_CORNER_BOUNDS
-  delete process.env.BOTTOM_RIGHT_CORNER_BOUNDS
+  delete process.env.TOP_RIGHT_CORNER_BOUNDS
+  delete process.env.BOTTOM_LEFT_CORNER_BOUNDS
 })
